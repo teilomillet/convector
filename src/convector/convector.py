f5c2ae0f1ba697dde11c0526926f2c412e1ade78
Teïlo M <39594466+teilomillet@users.noreply.github.com>
2023-11-09 19:12:33 +0100

    0.0.7c

--- src/convector/convector.py
+++ src/convector/convector.py
@@ -3,6 +3,7 @@ import os
 import logging
 from tqdm import tqdm
 from pathlib import Path
+from contextlib import contextmanager
 
 from convector.core.file_handler_factory import FileHandlerFactory
 from convector.utils.output_schema_handler import OutputSchemaHandler
@@ -11,21 +12,11 @@ from convector.core.config import ConvectorConfig, Profile
 
 logging.basicConfig(level=logging.INFO)
 
-class Convector:
-    """
-    Convector is the main class handling the transformation process of conversational data.
-    """
-    def __init__(self, profile: Profile, user_interaction, file_path):
-        """Initialization of the Convector object with necessary handlers and configurations."""
-        self.profile = profile 
-        self.user_interaction = user_interaction
-        self.file_handler = FileHandlerFactory.create_file_handler(file_path, profile)
-
-        logging.debug(f"Config object type: {type(self.profile)}")
-        logging.debug(f"Config object attributes: {dir(self.profile)}")
-
+class FileHandler:
+    def __init__(self, profile, file_handler):
+        self.profile = profile
+        self.file_handler = file_handler
         self.output_dir = profile.output_dir
-        self.output_schema_handler = OutputSchemaHandler(profile.output_schema, labels=profile.labels)
 
     def get_output_file_path(self):
         if hasattr(self.profile, 'output_file') and self.profile.output_file:
@@ -42,122 +33,145 @@ class Convector:
         print(f"Output will be saved to: {absolute_path}")
         return output_file_path
 
-    def write_to_file(self, file, item, lines_written, total_bytes_written, total_lines, bytes, output_schema_handler):
-        """
-        Write transformed items to the output file.
-        """
-        try:
-            if total_lines and lines_written >= total_lines:
-                return lines_written, total_bytes_written, True
-
-            if output_schema_handler is not None:  
-                transformed_item = output_schema_handler.apply_schema(item, labels=self.profile.labels)
-            else:
-                transformed_item = item
-
-            try:
-                if not isinstance(transformed_item, dict):
-                    print(f"Error: Expected a dictionary but got {type(transformed_item)}")
-                else:
-                    transformed_item['source'] = os.path.basename(self.file_handler.file_path)
-            except Exception as e:
-                print(f"An exception occurred: {e}")  # Debug
-                raise
-
-            json_line = json.dumps(transformed_item, ensure_ascii=False) + '\n'
-            line_bytes = len(json_line.encode('utf-8'))
-
-            if bytes and (total_bytes_written + line_bytes) > bytes:
-                return lines_written, total_bytes_written, True
-
-            file.write(json_line)
-            return lines_written + 1, total_bytes_written + line_bytes, False
-        except Exception as e:
-            logging.error(f"An error occurred while writing to the file: {e}")
-            raise
-
-    def process_and_save(self, transformed_data_generator, total_lines=None, bytes=None, append=False):
-        """
-        Process the transformed data and save it to the output file.
-        """
-        profile = self.profile
-        try:
-            progress_bar = None
-            output_file_path = self.get_output_file_path()
-            lines_written, total_bytes_written = 0, 0
-            
-            output_file_path.parent.mkdir(parents=True, exist_ok=True)
-
-            mode = 'a' if output_file_path.exists() and profile.append else 'w'
-            with open(output_file_path, mode, encoding='utf-8') as file:
-                total = profile.bytes or profile.lines or 0 
-                unit = " bytes" if profile.bytes else " lines"
-                progress_bar = tqdm(total=total, unit=unit, position=0, desc="Processing", leave=True)
-                
-                for items in transformed_data_generator:
-                    if isinstance(items, dict):
-                        items = [items]
-                    for item in items:
-                        lines_written, total_bytes_written, done = self.write_to_file(
-                            file, item, lines_written, total_bytes_written, total_lines, bytes, self.output_schema_handler
-                        )
-                        progress_bar.update(1)
-                        if done:
-                            break
-                
-                progress_bar.close()
-                
-            self.display_results(output_file_path, lines_written, total_bytes_written)
-            
-        except FileNotFoundError:
-            logging.error("Error: Output directory does not exist and could not be created.")
-        except IOError as e:
-            logging.error(f"IO Error: {e}")
-        except Exception as e:
-            logging.error(f"An unexpected error occurred while processing and saving the file: {e}")
-        finally:
-            if progress_bar:
-                progress_bar.close()
-
-    def display_results(self, output_file_path, lines_written, total_bytes_written):
-        print("Displaying results...")  # Debug
-        absolute_path = Path(output_file_path).resolve()
-        print(f"\nDelivered to file://{absolute_path} \n({lines_written} lines, {total_bytes_written} bytes)")
-
     def validate_input_file(self):
         if not os.path.exists(self.file_handler.file_path):
             logging.error(f"The file '{self.file_handler.file_path}' does not exist.")
             return False
         return True
+    
+    def display_results(self, output_file_path, lines_written, total_bytes_written):
+        print("Displaying results...")  # Debug
+        absolute_path = Path(output_file_path).resolve()
+        print(f"\nDelivered to file://{absolute_path} \n({lines_written} lines, {total_bytes_written} bytes)")
+
+class DataTransformer:
+    def __init__(self, profile, output_schema_handler):
+        self.profile = profile
+        self.output_schema_handler = output_schema_handler
+
+    def transform_item(self, item):
+        """
+        Apply the transformation schema to the item.
+        """
+        if self.output_schema_handler is not None:
+            transformed_item = self.output_schema_handler.apply_schema(item, labels=self.profile.labels)
+        else:
+            transformed_item = item
+
+        # You could add additional transformation logic here if needed.
+        # For example, if there are other fields that need to be modified or added,
+        # you could do so before returning the transformed item.
+
+        return transformed_item
+    
+class FileWriter:
+    def __init__(self, output_file_path, mode='a'):
+        self.output_file_path = output_file_path
+        self.mode = mode
+        self.buffer = []
+
+    def write_item(self, item):
+        self.buffer.append(json.dumps(item, ensure_ascii=False) + '\n')
+        if len(self.buffer) >= 100:  # Flush every 100 items
+            self.flush()
+
+    def flush(self):
+        with open(self.output_file_path, self.mode, encoding='utf-8') as file:
+            file.writelines(self.buffer)
+            self.mode = 'a'  # After the first write, always append
+        self.buffer.clear()
+
+    def close(self):
+        if self.buffer:
+            self.flush()
+
+@contextmanager
+def managed_progress_bar(total_lines):
+    progress_bar = tqdm(total=total_lines, unit=" lines", position=0, desc="Processing", leave=True)
+    try:
+        yield progress_bar
+    finally:
+        progress_bar.close()
+
+class DataSaver:
+    def __init__(self, profile, output_file_path, data_transformer):
+        self.profile = profile
+        self.output_file_path = output_file_path
+        self.data_transformer = data_transformer
+        self.file_writer = FileWriter(output_file_path)
+
+    def save_data(self, transformed_data_generator, total_lines, bytes, append):
+        lines_written = 0
+        total_bytes_written = 0
+
+        with managed_progress_bar(total_lines or 0) as progress_bar:
+            for items in transformed_data_generator:
+                if isinstance(items, dict):
+                    items = [items]
+                for item in items:
+                    transformed_item = self.data_transformer.transform_item(item)
+                    self.file_writer.write_item(transformed_item)
+
+                    lines_written += 1
+                    # Update the progress bar and bytes written as needed...
+                    progress_bar.update(1)
+
+                    if total_lines and lines_written >= total_lines:
+                        break
+                    # More conditions based on bytes or other stopping criteria...
+        
+        self.file_writer.close()  # Ensure the buffer is flushed at the end
+
+
+class ProcessingOrchestrator:
+    def __init__(self, profile, file_handler_module, data_transformer):
+        self.profile = profile
+        self.file_handler_module = file_handler_module
+        self.data_transformer = data_transformer
+
+    def orchestrate(self):
+        if not self.file_handler_module.validate_input_file():
+            return
+
+        logging.info("Starting processing...")
+        output_file_path = self.file_handler_module.get_output_file_path()
+        transformed_data_generator = self.file_handler_module.file_handler.handle_file()
+
+        data_saver = DataSaver(
+            self.profile, 
+            output_file_path, 
+            self.data_transformer
+        )
+        data_saver.save_data(
+            transformed_data_generator,
+            total_lines=self.profile.lines,
+            bytes=self.profile.bytes,
+            append=self.profile.append
+        )
+        logging.info("Data processing and saving complete.")
+
+class Convector:
+    """
+    Convector is the class that sets up the processing environment and starts the process.
+    """
+    def __init__(self, profile: Profile, user_interaction, file_path):
+        """
+        Initialization of the Convector object with necessary handlers and configurations.
+        """
+        self.profile = profile 
+        self.user_interaction = user_interaction
+        # FileHandlerFactory creates an appropriate file handler based on the file_path and profile.
+        self.file_handler = FileHandlerFactory.create_file_handler(file_path, profile)
+        # OutputSchemaHandler is initialized here and will be passed where needed.
+        self.output_schema_handler = OutputSchemaHandler(profile.output_schema, labels=profile.labels)
+
+        # The FileHandler module handles file validation and getting the output file path.
+        self.file_handler_module = FileHandler(profile, self.file_handler)
+        # The DataTransformer module will be responsible for transforming the data according to the schema.
+        self.data_transformer = DataTransformer(profile, self.output_schema_handler)
 
     def process(self):
-        """
-        Processes the input file and writes the output to a file.
-        This method orchestrates the entire process, from reading the input,
-        processing the data, and writing the output.
-        """
-        try:
-            if not self.validate_input_file():
-                return
-            
-            logging.info("Starting processing...")
+        # Processing is now delegated to the ProcessingOrchestrator.
+        orchestrator = ProcessingOrchestrator(self.profile, self.file_handler_module, self.data_transformer)
+        orchestrator.orchestrate()
 
-            output_file_path = self.get_output_file_path()
-            output_file_path.parent.mkdir(parents=True, exist_ok=True)
-
-            transformed_data_generator = self.file_handler.handle_file()
-
-            logging.info("Data generation complete...")
-
-            self.process_and_save(
-                transformed_data_generator,
-                total_lines = self.profile.lines,
-                bytes= self.profile.bytes,
-                append=self.profile.append,
-            )
-
-            logging.info("Data processing and saving complete...")
-        except FileNotFoundError:
-            logging.error(f"The file '{self.file_handler.file_path}' does not exist.")
-        except Exception as e:
-            logging.error(f"An unexpected error occurred while transforming the file: {e}")
